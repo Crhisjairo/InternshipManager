0dc77f7ee960b11bd801b2df6345f6e70236d351
Adriana Galperin <45876950+aagalperin@users.noreply.github.com>
2021-11-11 11:59:03 -0500

    Add layout
    - Drawer Menu
    - Fragments for calendar, internship list, settings, info and logout
    - layout for internship items

--- app/src/main/java/ca/qc/bdeb/internshipmanager/MainActivity.java
+++ app/src/main/java/ca/qc/bdeb/internshipmanager/MainActivity.java
@@ -1,8 +1,26 @@
 package ca.qc.bdeb.internshipmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
+
+import ca.qc.bdeb.internshipmanager.fragments.CalendarFragment;
+import ca.qc.bdeb.internshipmanager.fragments.InfoFragment;
+import ca.qc.bdeb.internshipmanager.fragments.ListInternshipFragment;
+import ca.qc.bdeb.internshipmanager.fragments.LogoutFragment;
+import ca.qc.bdeb.internshipmanager.fragments.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +28,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialToolbar toolbar = findViewById(R.id.topAppbar);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                item.setChecked(true);
+                drawerLayout.closeDrawer(GravityCompat.START);
+                switch (id){
+                    case R.id.nav_internship_list:
+                        replaceFragment(new ListInternshipFragment());
+                        break;
+                    case R.id.nav_map:
+                        Toast.makeText(MainActivity.this, "Maps is currently clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_calendar:
+                        replaceFragment(new CalendarFragment());
+                        break;
+                    case R.id.nav_settings:
+                        replaceFragment(new SettingsFragment());
+                        break;
+                    case R.id.nav_logout:
+                        replaceFragment(new LogoutFragment());
+                        break;
+                    case R.id.nav_info:
+                        replaceFragment(new InfoFragment());
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
