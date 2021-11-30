b2d9b1c162a3cbf1c3226abf9f13590ec4477ad4
Christian C <crhisjairo@hotmail.com>
2021-11-29 20:20:08 -0500

    Ajout du layout du calendrier

--- app/src/main/java/ca/qc/bdeb/internshipmanager/fragments/CalendarFragment.java
+++ app/src/main/java/ca/qc/bdeb/internshipmanager/fragments/CalendarFragment.java
@@ -1,12 +1,24 @@
 package ca.qc.bdeb.internshipmanager.fragments;
 
+import android.graphics.RectF;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewEvent;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Random;
 
 import ca.qc.bdeb.internshipmanager.R;
 
@@ -17,14 +29,7 @@ import ca.qc.bdeb.internshipmanager.R;
  */
 public class CalendarFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private WeekView weekView;
 
     public CalendarFragment() {
         // Required empty public constructor
@@ -34,16 +39,13 @@ public class CalendarFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment CalendarFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CalendarFragment newInstance(String param1, String param2) {
+    public static CalendarFragment newInstance() {
         CalendarFragment fragment = new CalendarFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        //Paramètre si on a besoin ici
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,9 +54,10 @@ public class CalendarFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            //set arguments ici
         }
+
+
     }
 
     @Override
@@ -63,4 +66,23 @@ public class CalendarFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calendar, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        weekView = (WeekView) view.findViewById(R.id.weekView);
+
+        configCalendar();
+    }
+
+    private void configCalendar() {
+        weekView.setAddEventClickListener(new WeekView.AddEventClickListener() {
+            @Override
+            public void onAddEventClicked(@NonNull Calendar calendar, @NonNull Calendar calendar1) {
+                Toast.makeText(getContext(), "calendar", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
